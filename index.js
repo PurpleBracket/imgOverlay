a2fa4d99669c39c8ba0c445601d1e7bffdd54060
PurpleBracket <mynameisalimalik@gmail.com>
2018-03-26 15:22:35 +0300

    adds jimp for image manipulation

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 const mergeImages = require('merge-images');
 const Canvas = require('canvas');
 const fs = require('fs');
-const sys = require('sys');
+const Jimp = require("jimp");
 
-mergeImages(['./body.png', './eyes.png', './mouth.png'], {
-  Canvas: Canvas
-}).then(b64 => {
-  // console.log(b64);
-  var data = b64.replace(/^data:image\/\w+;base64,/, "");
-  var buf = new Buffer(data, 'base64');
-  fs.writeFile('test.png', buf);
-  return
+//
+// Jimp.read("body.png", function (err, img) {
+//   if (err) throw err;
+//   img.resize(256, 256)            // resize
+//     .quality(60)                 // set JPEG quality
+//     .greyscale()                 // set greyscale
+//     .write("body-bw.png"); // save
+// });
+
+Jimp.read("body.png").then(function (img) {
+  img.resize(256, 256)            // resize
+  // img.resize(512, 512)            // resize 512
+    // .quality(60)                 // set JPEG quality
+    .greyscale()                 // set greyscale
+    .write("body-bw.png", function (err) {
+      mergeImages(['./body-bw.png', './eyes.png', './mouth.png'], {
+        Canvas: Canvas
+      }).then((b64) => {
+        // console.log(b64);
+        var data = b64.replace(/^data:image\/\w+;base64,/, "");
+        var buf = new Buffer(data, 'base64');
+        fs.writeFile('test.png', buf);
+        return
+      });
+    }); // save
+}).catch(function (err) {
+  if (err) throw err;
 });
+
+// mergeImages(['./body-bw.png', './eyes.png', './mouth.png'], {
+//   Canvas: Canvas
+// }).then(b64 => {
+//   // console.log(b64);
+//   var data = b64.replace(/^data:image\/\w+;base64,/, "");
+//   var buf = new Buffer(data, 'base64');
+//   fs.writeFile('test.png', buf);
+//   return
+// });
 
